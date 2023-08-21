e7f7d1a8d89b57ab075de142ccf0925ded2bd68e
Diego Teixeira <127033805+Diego-Ted-Teixeira@users.noreply.github.com>
2023-08-21 13:19:44 -0300

    desafio-sistema-bancario
    Desafio Sistema bancário Bootcamp Ciência de Dados com Python.
    Este desafio emprega os aprendizados iniciais linguagem Python, onde deve ser criado um sistema simples que possa fazer o controle de deposito e saque assim como a impressão de extrato mostrando valor atual, valor depositado, valor sacado e quantidade de saques efetuados.

---
+++ desafio-sistema-bancario.py
@@ -0,0 +1,60 @@
+menu = '''
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=>
+'''
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+    
+    if opcao == "d":
+        valor = float(input("Informe o valor a ser depositado: "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor: .2f}\n"
+           
+        else:
+            print("Falha na operação! O valor informado é inválido para essa operação")    
+            
+    elif opcao == "s":
+        valor = float(input("Informe o valor qeu deseja sacar: "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+        if excedeu_saldo:
+            print("Falha na operação! Você não tem saldo suficiente para essa transação.")
+        elif excedeu_limite:
+            print("Falha na operação! Você não tem limite suficiente para essa transação.")   
+        elif excedeu_saques:
+            print("Falha na operação! Numero máximo de saques excedido.")  
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Falha na operação! Valor informado é inválido")
+            
+    elif opcao == "e":
+        print("\n====== EXTRATO ======")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=========================================")
+    elif opcao =="q":
+        break
+    else:
+        print("Operação inválida, por favor selecione a operação desejada.")    
+                
+    
+                   
+                
